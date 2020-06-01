1190d1028bcbbe130bcd862047c7c40c1b48e5c9
dherv <damien.herv@gmail.com>
2020-06-02 08:01:04 +0900

    feat(job-title): increase global styles with padding and dark color

--- src/styled/Heading.ts
+++ src/styled/Heading.ts
@@ -3,3 +3,7 @@ import styled, { css } from "styled-components";
 export const Heading = styled.h1`
   font-weight: 600;
 `;
+
+export const HeadingDark = styled(Heading)`
+  color: var(--text-color-dark);
+`;
