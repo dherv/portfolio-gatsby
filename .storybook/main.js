dac8cb5156a65e11c643dd268ded494e00944a8e
dherv <damien.herv@gmail.com>
2020-06-02 07:13:02 +0900

    style: format storybook files

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,28 +5,28 @@ module.exports = {
     "../src/**/*.stories.tsx",
   ],
   addons: ["@storybook/addon-actions", "@storybook/addon-links"],
-  webpackFinal: async config => {
+  webpackFinal: async (config) => {
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/];
 
     // use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
-    config.module.rules[0].use[0].loader = require.resolve("babel-loader")
+    config.module.rules[0].use[0].loader = require.resolve("babel-loader");
 
     // use @babel/preset-react for JSX and env (instead of staged presets)
     config.module.rules[0].use[0].options.presets = [
       require.resolve("@babel/preset-react"),
       require.resolve("@babel/preset-env"),
-    ]
+    ];
 
     config.module.rules[0].use[0].options.plugins = [
       // use @babel/plugin-proposal-class-properties for class arrow functions
       require.resolve("@babel/plugin-proposal-class-properties"),
       // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
       require.resolve("babel-plugin-remove-graphql-queries"),
-    ]
+    ];
 
     // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
-    config.resolve.mainFields = ["browser", "module", "main"]
+    config.resolve.mainFields = ["browser", "module", "main"];
 
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
@@ -39,10 +39,26 @@ module.exports = {
           require.resolve("babel-plugin-remove-graphql-queries"),
         ],
       },
-    })
+    });
 
-    config.resolve.extensions.push(".ts", ".tsx")
+    config.resolve.extensions.push(".ts", ".tsx");
 
-    return config
+    // CSS MODULES configuration
+    const ruleCssIndex = config.module.rules.findIndex(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    );
+
+    // map over the 'use' array of the css rule and set the 'module' option to true
+    config.module.rules[ruleCssIndex].use.map((item) => {
+      if (item.loader && item.loader.includes("/css-loader/")) {
+        item.options.modules = {
+          mode: "local",
+          localIdentName: "[name]__[local]--[hash:base64:5]",
+        };
+      }
+      return item;
+    });
+
+    return config;
   },
-}
+};
