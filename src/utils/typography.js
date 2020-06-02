4aef9cadc2ab3db2315e5fefaa68fa39242ed97e
dherv <damien.herv@gmail.com>
2020-06-03 08:53:18 +0900

    chore(typography): update typography with Roboto and Barlow Condensed

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,37 @@
-import Typography from "typography"
-import kirkhamTheme from "typography-theme-kirkham"
+import Typography from "typography";
 
-const typography = new Typography(kirkhamTheme)
+const typography = new Typography({
+  baseFontSize: "12px",
+  baseLineHeight: 1.5,
+  headerFontFamily: [
+    "Barlow Condensed",
+    "Roboto",
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  bodyFontFamily: [
+    "Roboto",
+    "Avenir Next",
+    "Helvetica Neue",
+    "Segoe UI",
+    "Helvetica",
+    "Arial",
+    "sans-serif",
+  ],
+  googleFonts: [
+    {
+      name: "Barlow Condensed",
+      styles: ["500", "600", "700"],
+    },
+    {
+      name: "Roboto",
+      styles: ["400", "400i", "700", "700i"],
+    },
+  ],
+});
 
-export default typography
-export const rhythm = typography.rhythm
+export default typography;
